6001ef5c287ffbfa7de8e1d13c9f35098a2da0a3
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2022-02-18 16:46:10 -0500

    blank:

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,26 +1,20 @@
 // build your `/api/resources` router here
 const express = require('express')
-const Tasks = require('./model')
+const Resources = require('./model')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    Tasks.getAll()
-    .then((task) => {
-        res.status(200).json(task)
+    Resources.getAll()
+    .then(resource => {
+        res.status(200).json(resource)
     })
     .catch(next)
 })
 
-router.post('/', async (req, res, next) => { 
+router.post('/', async (req, res, next) => {
     try{
-        const newTask = await Tasks.create(req.body)
-        res.status(201).json({
-            task_id: newTask.task_id,
-            task_description: newTask.task_description,
-            task_notes: newTask.task_notes,
-            task_completed: newTask.task_completed === 0 ? false : true,
-            project_id: newTask.project_id
-        })
+        const newResource = await Resources.create(req.body)
+        res.status(201).json(newResource)
     }catch(err){
         next(err)
     }
